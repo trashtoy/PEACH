3ab03d1c7e159992e92f64beccd3b6a14ad41027
trashtoy <trashtoy@yahoo.co.jp>
2013-11-04 22:42:50 +0900

    Added Peach_Markup_NoneTest

---
+++ test/Peach/Markup/Peach_Markup_NoneTest.php
@@ -0,0 +1,54 @@
+<?php
+class Peach_Markup_NoneTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Peach_Markup_None
+     */
+    protected $object;
+    
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = Peach_Markup_None::getInstance();
+    }
+    
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+    
+    /**
+     * getInstance() のテストです.
+     * 
+     * - Peach_Markup_None のインスタンスを返すことを確認します.
+     * - 常に同一のオブジェクトを返すことを確認します.
+     * 
+     * @covers Peach_Markup_None::getInstance
+     */
+    public function testGetInstance()
+    {
+        $obj = Peach_Markup_None::getInstance();
+        $this->assertSame("Peach_Markup_None", get_class($obj));
+        $this->assertSame($this->object, $obj);
+    }
+    
+    /**
+     * Context の handleNone() が呼び出されることを確認します.
+     * 
+     * @covers Peach_Markup_None::accept
+     */
+    public function testAccept()
+    {
+        $obj   = $this->object;
+        $debug = new Peach_Markup_DebugContext(false);
+        
+        $obj->accept($debug);
+        $this->assertSame("None\r\n", $debug->getResult());
+    }
+}
