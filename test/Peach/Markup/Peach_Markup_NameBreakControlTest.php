a4dec75eab392411f1e5b8a794cba8741db7dc0a
trashtoy <trashtoy@yahoo.co.jp>
2014-03-25 23:16:32 +0900

    Implemented tests for Peach_Markup_NameBreakControl

--- test/Peach/Markup/Peach_Markup_NameBreakControlTest.php
+++ test/Peach/Markup/Peach_Markup_NameBreakControlTest.php
@@ -12,7 +12,7 @@ class Peach_Markup_NameBreakControlTest extends PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $this->object = new Peach_Markup_NameBreakControl(array("A", "B", "C"), array("X", "Y", "Z"));
+        $this->object = new Peach_Markup_NameBreakControl(array("A", "B", "C", "Q"), array("Q", "X", "Y", "Z"));
     }
 
     /**
@@ -25,14 +25,44 @@ class Peach_Markup_NameBreakControlTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * break() のテストです. 以下を確認します.
+     * 
+     * - 強制改行する要素の場合, オリジナルの条件を無視して改行します
+     * - 強制改行しない要素の場合, オリジナルの条件を無視して改行しないようにします
+     * - どちらにも属する要素の場合, 強制改行のほうが優先されます.
+     * - どちらにも属さない要素の場合, オリジナルの改行ルールに従います
+     * 
      * @covers Peach_Markup_NameBreakControl::breaks
-     * @todo   Implement testBreaks().
      */
     public function testBreaks()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $c = $this->object;
+        
+        $this->assertTrue($c->breaks($this->createBreakNode("A")));
+        $this->assertTrue($c->breaks($this->createNoBreakNode("B")));
+        
+        $this->assertFalse($c->breaks($this->createBreakNode("X")));
+        $this->assertFalse($c->breaks($this->createNoBreakNode("Y")));
+        
+        $this->assertTrue($c->breaks($this->createBreakNode("Q")));
+        $this->assertTrue($c->breaks($this->createNoBreakNode("Q")));
+        
+        $this->assertTrue($c->breaks($this->createBreakNode("T")));
+        $this->assertFalse($c->breaks($this->createNoBreakNode("T")));
+    }
+    
+    private function createBreakNode($name)
+    {
+        $test = new Peach_Markup_ContainerElement($name);
+        $test->append("First Child");
+        $test->append("Second Child");
+        return $test;
+    }
+    
+    private function createNoBreakNode($name)
+    {
+        $test = new Peach_Markup_ContainerElement($name);
+        $test->append("First Child");
+        return $test;
     }
 }
