ff518c8134cdf1308f332e7a70d5f3ecc333e690
trashtoy <trashtoy@yahoo.co.jp>
2014-03-30 21:03:18 +0900

    Implemented tests for Peach_Markup_DebugContext

--- src/Peach/Markup/DebugContext.php
+++ src/Peach/Markup/DebugContext.php
@@ -23,31 +23,33 @@
 /** @package Markup */
 /**
  * デバッグのために使用される Context です.
- * この Context の出力結果は, ノードツリーの構造を可視化した文字列となります.
+ * この Context は, handle メソッドを実行した時に自動で echo を行います.
+ * 出力内容は, ノードツリーの構造を可視化した文字列となります.
  * @package Markup
  */
 class Peach_Markup_DebugContext extends Peach_Markup_Context
 {
     /**
-     *
+     * 出力を整形するために使用される Indent です.
      * @var Peach_Markup_Indent
      */
     private $indent;
     
     /**
-     *
+     * 出力結果です.
      * @var string
      */
     private $result;
     
     /**
-     *
+     * true の場合, handle() を実行すると同時に echo を実行します.
      * @var bool
      */
     private $echoMode;
     
     /**
-     * 
+     * 新しい DebugContext を構築します.
+     * @param bool $echoMode 自動で出力を行うかどうかの設定 (デフォルトで true)
      */
     public function __construct($echoMode = true)
     {
@@ -57,7 +59,6 @@ class Peach_Markup_DebugContext extends Peach_Markup_Context
     }
     
     /**
-     * 
      * @param string $name
      */
     private function startNode($name)
@@ -70,7 +71,7 @@ class Peach_Markup_DebugContext extends Peach_Markup_Context
     }
     
     /**
-     * 
+     * インデントレベルを下げて "}" を出力します.
      */
     private function endNode()
     {
@@ -81,6 +82,9 @@ class Peach_Markup_DebugContext extends Peach_Markup_Context
         $this->result .= $result;
     }
     
+    /**
+     * @param string $contents
+     */
     private function append($contents)
     {
         $result = $this->indent->indent() . $contents . $this->indent->breakCode();
@@ -90,6 +94,9 @@ class Peach_Markup_DebugContext extends Peach_Markup_Context
         $this->result .= $result;
     }
     
+    /**
+     * @param Peach_Markup_Container $container
+     */
     private function handleContainer(Peach_Markup_Container $container)
     {
         foreach ($container->getChildNodes() as $node) {
@@ -97,18 +104,46 @@ class Peach_Markup_DebugContext extends Peach_Markup_Context
         }
     }
     
+    /**
+     * デバッグ用の出力データを返します.
+     * @return string
+     */
     public function getResult()
     {
         return $this->result;
     }
     
+    /**
+     * Comment ノードのデバッグ文字列を出力します.
+     * 出力内容は以下の通りです.
+     * 
+     * <pre>
+     * Comment {
+     *     # 子ノードの出力内容
+     * }
+     * </pre>
+     * 
+     * @param Peach_Markup_Comment $node
+     */
     public function handleComment(Peach_Markup_Comment $node)
     {
         $this->startNode("Comment");
         $this->handleContainer($node);
         $this->endNode();
     }
-
+    
+    /**
+     * ContainerElement ノードのデバッグ文字列を出力します.
+     * 出力内容は以下の通りです.
+     * 
+     * <pre>
+     * ContainerElement(tagName) {
+     *     # 子ノードの出力内容
+     * }
+     * </pre>
+     * 
+     * @param Peach_Markup_ContainerElement $node
+     */
     public function handleContainerElement(Peach_Markup_ContainerElement $node)
     {
         $name = $node->getName();
@@ -116,30 +151,67 @@ class Peach_Markup_DebugContext extends Peach_Markup_Context
         $this->handleContainer($node);
         $this->endNode();
     }
-
+    
+    /**
+     * EmptyElement ノードのデバッグ文字列を出力します.
+     * 出力内容は以下の通りです.
+     * 
+     * <pre>
+     * EmptyElement(tagName)
+     * </pre>
+     * 
+     * @param Peach_Markup_EmptyElement $node
+     */
     public function handleEmptyElement(Peach_Markup_EmptyElement $node)
     {
         $name = $node->getName();
         $this->append("EmptyElement({$name})");
     }
-
+    
+    /**
+     * NodeList のデバッグ文字列を出力します.
+     * 出力内容は以下の通りです.
+     * 
+     * <pre>
+     * NodeList {
+     *     # 子ノードの出力内容
+     * }
+     * </pre>
+     * 
+     * @param Peach_Markup_NodeList $node
+     */
     public function handleNodeList(Peach_Markup_NodeList $node)
     {
         $this->startNode("NodeList");
         $this->handleContainer($node);
         $this->endNode();
     }
-
+    
+    /**
+     * Text ノードのデバッグ文字列を出力します.
+     * 出力内容は文字列 "Text" です.
+     * @param Peach_Markup_Text $node
+     */
     public function handleText(Peach_Markup_Text $node)
     {
         $this->append("Text");
     }
-
+    
+    /**
+     * Code ノードのデバッグ文字列を出力します.
+     * 出力内容は文字列 "Code" です.
+     * @param Peach_Markup_Code $node
+     */
     public function handleCode(Peach_Markup_Code $node)
     {
         $this->append("Code");
     }
     
+    /**
+     * None のデバッグ文字列を出力します.
+     * 出力内容は文字列 "None" です.
+     * @param Peach_Markup_None $none
+     */
     public function handleNone(Peach_Markup_None $none)
     {
         $this->append("None");
--- test/Peach/Markup/Peach_Markup_DebugContextTest.php
+++ test/Peach/Markup/Peach_Markup_DebugContextTest.php
@@ -1,12 +1,14 @@
 <?php
 require_once(__DIR__ . "/Peach_Markup_ContextTest.php");
+require_once(__DIR__ . "/Peach_Markup_TestUtil.php");
+
 class Peach_Markup_DebugContextTest extends Peach_Markup_ContextTest
 {
     /**
      * @var Peach_Markup_DebugContext
      */
     protected $object;
-
+    
     /**
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
@@ -16,7 +18,7 @@ class Peach_Markup_DebugContextTest extends Peach_Markup_ContextTest
         parent::setUp();
         $this->object   = new Peach_Markup_DebugContext();
     }
-
+    
     /**
      * Tears down the fixture, for example, closes a network connection.
      * This method is called after a test is executed.
@@ -26,84 +28,167 @@ class Peach_Markup_DebugContextTest extends Peach_Markup_ContextTest
     }
     
     /**
+     * getResult() のテストです.
+     * 各ノードのクラス名とその入れ子構造が出力されることを確認します.
+     * 
      * @covers Peach_Markup_DebugContext::getResult
-     * @todo   Implement testGetResult().
      */
     public function testGetResult()
     {
-
-        $context = new Peach_Markup_DebugContext();
+        $expected = implode("\r\n", array(
+            "ContainerElement(html) {",
+            "    ContainerElement(head) {",
+            "        EmptyElement(meta)",
+            "        ContainerElement(title) {",
+            "            Text",
+            "        }",
+            "    }",
+            "    ContainerElement(body) {",
+            "        ContainerElement(form) {",
+            "            Text",
+            "            EmptyElement(input)",
+            "            EmptyElement(br)",
+            "            EmptyElement(input)",
+            "            Text",
+            "            EmptyElement(br)",
+            "            EmptyElement(input)",
+            "        }",
+            "    }",
+            "}",
+        )) . "\r\n";
+        $node    = Peach_Markup_TestUtil::getTestNode();
+        $context = $this->object;
+        $this->expectOutputString($expected);
+        $context->handle($node);
+        $this->assertSame($expected, $context->getResult());
     }
     
     /**
+     * handleComment() のテストです.
      * @covers Peach_Markup_DebugContext::handleComment
-     * @todo   Implement testHandleComment().
      */
     public function testHandleComment()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $expected = implode("\r\n", array(
+            "Comment {",
+            "    Text",
+            "}",
+        )) . "\r\n";
+        $context   = $this->object;
+        $comment   = new Peach_Markup_Comment();
+        $comment->append("This is test");
+        $this->expectOutputString($expected);
+        $context->handleComment($comment);
+        $this->assertSame($expected, $context->getResult());
     }
-
+    
     /**
+     * handleContainerElement() のテストです.
      * @covers Peach_Markup_DebugContext::handleContainerElement
-     * @todo   Implement testHandleContainerElement().
      */
     public function testHandleContainerElement()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $expected = implode("\r\n", array(
+            "ContainerElement(div) {",
+            "    Text",
+            "}",
+        )) . "\r\n";
+        $context   = $this->object;
+        $container = new Peach_Markup_ContainerElement("div");
+        $container->append("This is test");
+        $this->expectOutputString($expected);
+        $context->handleContainerElement($container);
+        $this->assertSame($expected, $context->getResult());
     }
-
+    
     /**
+     * handleEmptyElement() のテストです.
      * @covers Peach_Markup_DebugContext::handleEmptyElement
-     * @todo   Implement testHandleEmptyElement().
      */
     public function testHandleEmptyElement()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $expected = "EmptyElement(br)\r\n";
+        $context  = $this->object;
+        $emp      = new Peach_Markup_EmptyElement("br");
+        $this->expectOutputString($expected);
+        $context->handleEmptyElement($emp);
+        $this->assertSame($expected, $context->getResult());
     }
-
+    
     /**
+     * handleNodeList() のテストです.
      * @covers Peach_Markup_DebugContext::handleNodeList
-     * @todo   Implement testHandleNodeList().
      */
     public function testHandleNodeList()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $expected = implode("\r\n", array(
+            "NodeList {",
+            "    Comment {",
+            "        Text",
+            "    }",
+            "    EmptyElement(img)",
+            "    Text",
+            "}",
+        )) . "\r\n";
+        $nodeList = new Peach_Markup_NodeList();
+        $comment  = new Peach_Markup_Comment();
+        $comment->append("This is test comment");
+        $img      = new Peach_Markup_EmptyElement("img");
+        $img->setAttributes(array("src" => "test.jpg", "alt" => ""));
+        $nodeList->append($comment);
+        $nodeList->append($img);
+        $nodeList->append("Test image");
+        
+        $context = $this->object;
+        $this->expectOutputString($expected);
+        $context->handleNodeList($nodeList);
+        $this->assertSame($expected, $context->getResult());
     }
-
+    
     /**
+     * handleText() のテストです.
      * @covers Peach_Markup_DebugContext::handleText
-     * @todo   Implement testHandleText().
      */
     public function testHandleText()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $expected = "Text\r\n";
+        $context  = $this->object;
+        $text     = new Peach_Markup_Text("This is test");
+        $this->expectOutputString($expected);
+        $context->handleText($text);
+        $this->assertSame($expected, $context->getResult());
     }
-
+    
     /**
+     * handleCode() のテストです.
      * @covers Peach_Markup_DebugContext::handleCode
-     * @todo   Implement testHandleCode().
      */
     public function testHandleCode()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $expected = "Code\r\n";
+        $context  = $this->object;
+        $str      = implode("\n", array(
+            "$(function() {",
+            "    $(\"#navi\").hide();",
+            "}",
+        ));
+        $code     = new Peach_Markup_Code($str);
+        $this->expectOutputString($expected);
+        $context->handleCode($code);
+        $this->assertSame($expected, $context->getResult());
+    }
+    
+    /**
+     * handleNone() のテストです.
+     * @covers Peach_Markup_DebugContext::handleNone
+     */
+    public function testHandleNone()
+    {
+        $expected = "None\r\n";
+        $context  = $this->object;
+        $none     = Peach_Markup_None::getInstance();
+        $this->expectOutputString($expected);
+        $context->handleNone($none);
+        $this->assertSame($expected, $context->getResult());
     }
 }
