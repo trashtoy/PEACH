9f99043811e00ad873f6e61be85cce32f6c80e61
trashtoy <trashtoy@yahoo.co.jp>
2014-05-06 20:48:41 +0900

    Added Peach_Markup_MinimalBreakControl

---
+++ src/Peach/Markup/MinimalBreakControl.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * Copyright (c) 2014 @trashtoy
+ * https://github.com/trashtoy/
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/** @package Markup */
+/**
+ * 一切の改行をしない BreakControl です.
+ * この BreakControl を使ってノードの出力を行った場合, 出力結果には改行文字 CR または LF
+ * が出現せず, 1 行のコードが生成されます.
+ * 
+ * ただし整形済テキスト ({@link Peach_Markup_Code Code}) については例外です.
+ * テキストに含まれる改行文字をありのまま出力します.
+ * 
+ * @package Markup
+ */
+class Peach_Markup_MinimalBreakControl implements Peach_Markup_BreakControl
+{
+    /**
+     * この実装は常に false を返します.
+     * @param Peach_Markup_ContainerElement $node
+     * @return bool false
+     */
+    public function breaks(Peach_Markup_ContainerElement $node)
+    {
+        return false;
+    }
+    
+    /**
+     * 唯一のインスタンスを取得します.
+     * 
+     * @return Peach_Markup_MinimalBreakControl
+     */
+    public static function getInstance()
+    {
+        static $instance = null;
+        if ($instance === null) {
+            $instance = new self();
+        }
+        return $instance;
+    }
+}
