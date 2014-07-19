f7d1cac87647a0b7716052c697ab61c2b2b1bc55
trashtoy <trashtoy@yahoo.co.jp>
2014-07-19 15:03:43 +0900

    Modified test for SimpleFormat
    Detail:
    - Added: Peach_DT_SimpleFormatTest_FormatData

--- test/Peach/DT/Peach_DT_SimpleFormatTest.php
+++ test/Peach/DT/Peach_DT_SimpleFormatTest.php
@@ -5,11 +5,6 @@
 class Peach_DT_SimpleFormatTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * @var array
-     */
-    private $testFormats;
-    
-    /**
      *
      * @var array
      */
@@ -21,13 +16,6 @@ class Peach_DT_SimpleFormatTest extends PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        if ($this->testFormats === null) {
-            $this->testFormats = $this->createObject(array(
-                array("YmdHis", "Y年n月j日G時f分b秒", "\\Y=Y \\n=n \\j=j \\H=H \\i=i \\s=s"),
-                array("Y/m/d",  "Y.n.j"),
-                array("H:i:s",  "G時f分b秒")
-            ));
-        }
         if ($this->sampleList === null) {
             $d1 = new Peach_DT_Date(2012, 5, 21);
             $d2 = new Peach_DT_Datetime(2012, 5, 21, 7, 30);
@@ -39,33 +27,47 @@ class Peach_DT_SimpleFormatTest extends PHPUnit_Framework_TestCase
             $d8 = Peach_DT_Datetime::now()->setAll(array("hour" => 8, "minute" => 6));
             $d9 = Peach_DT_Timestamp::now()->setAll(array("hour" => 8, "minute" => 6, "second" => 4));
             $this->sampleList = array(
-                new Peach_DT_SimpleFormatTest_Sample(
-                    "YmdHis",
-                    new Peach_DT_SimpleFormatTest_ParseData("20120521073009",                 "2012052112345",                $d1, $d2, $d3)
+                new Peach_DT_SimpleFormatTest_Sample("YmdHis",
+                    new Peach_DT_SimpleFormatTest_ParseData("20120521073009", "2012052112345", $d1, $d2, $d3),
+                    new Peach_DT_SimpleFormatTest_FormatData($d1, "20120521000000"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d2, "20120521073000"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d3, "20120521073009")
                 ),
-                new Peach_DT_SimpleFormatTest_Sample(
-                    "Y年n月j日G時f分b秒",
-                    new Peach_DT_SimpleFormatTest_ParseData("2012年5月21日7時30分9秒",        "2012年05月21日07時30分09秒",   $d1, $d2, $d3)
+                new Peach_DT_SimpleFormatTest_Sample("Y年n月j日G時f分b秒",
+                    new Peach_DT_SimpleFormatTest_ParseData("2012年5月21日7時30分9秒", "2012年05月21日07時30分09秒", $d1, $d2, $d3),
+                    new Peach_DT_SimpleFormatTest_FormatData($d1, "2012年5月21日0時0分0秒"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d2, "2012年5月21日7時30分0秒"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d3, "2012年5月21日7時30分9秒")
                 ),
-                new Peach_DT_SimpleFormatTest_Sample(
-                    "\\Y=Y \\n=n \\j=j \\H=H \\i=i \\s=s",
-                    new Peach_DT_SimpleFormatTest_ParseData("Y=2012 n=5 j=21 H=07 i=30 s=09", "Y=2012 n=5 j=21 H=7 i=30 s=9", $d1, $d2, $d3)
+                new Peach_DT_SimpleFormatTest_Sample("\\Y=Y \\n=n \\j=j \\H=H \\i=i \\s=s",
+                    new Peach_DT_SimpleFormatTest_ParseData("Y=2012 n=5 j=21 H=07 i=30 s=09", "Y=2012 n=5 j=21 H=7 i=30 s=9", $d1, $d2, $d3),
+                    new Peach_DT_SimpleFormatTest_FormatData($d1, "Y=2012 n=5 j=21 H=00 i=00 s=00"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d2, "Y=2012 n=5 j=21 H=07 i=30 s=00"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d3, "Y=2012 n=5 j=21 H=07 i=30 s=09")
                 ),
-                new Peach_DT_SimpleFormatTest_Sample(
-                    "Y/m/d",
-                    new Peach_DT_SimpleFormatTest_ParseData("2012/03/07", "2012-03-07", $d4, $d5, $d6)
+                new Peach_DT_SimpleFormatTest_Sample("Y/m/d",
+                    new Peach_DT_SimpleFormatTest_ParseData("2012/03/07", "2012-03-07", $d4, $d5, $d6),
+                    new Peach_DT_SimpleFormatTest_FormatData($d1, "2012/05/21"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d2, "2012/05/21"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d3, "2012/05/21")
                 ),
-                new Peach_DT_SimpleFormatTest_Sample(
-                    "Y.n.j",
-                    new Peach_DT_SimpleFormatTest_ParseData("2012.3.7",   "hogehoge",   $d4, $d5, $d6)
+                new Peach_DT_SimpleFormatTest_Sample("Y.n.j",
+                    new Peach_DT_SimpleFormatTest_ParseData("2012.3.7",   "hogehoge",   $d4, $d5, $d6),
+                    new Peach_DT_SimpleFormatTest_FormatData($d1, "2012.5.21"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d2, "2012.5.21"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d3, "2012.5.21")
                 ),
-                new Peach_DT_SimpleFormatTest_Sample(
-                    "H:i:s",
-                    new Peach_DT_SimpleFormatTest_ParseData("08:06:04",   "8:06:04",    $d7, $d8, $d9)
+                new Peach_DT_SimpleFormatTest_Sample("H:i:s",
+                    new Peach_DT_SimpleFormatTest_ParseData("08:06:04",   "8:06:04",    $d7, $d8, $d9),
+                    new Peach_DT_SimpleFormatTest_FormatData($d1, "00:00:00"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d2, "07:30:00"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d3, "07:30:09")
                 ),
-                new Peach_DT_SimpleFormatTest_Sample(
-                    "G時f分b秒",
-                    new Peach_DT_SimpleFormatTest_ParseData("8時6分4秒",  "8じ6分4秒",  $d7, $d8, $d9)
+                new Peach_DT_SimpleFormatTest_Sample("G時f分b秒",
+                    new Peach_DT_SimpleFormatTest_ParseData("8時6分4秒",  "8じ6分4秒",  $d7, $d8, $d9),
+                    new Peach_DT_SimpleFormatTest_FormatData($d1, "0時0分0秒"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d2, "7時30分0秒"),
+                    new Peach_DT_SimpleFormatTest_FormatData($d3, "7時30分9秒")
                 ),
             );
         }
@@ -77,15 +79,6 @@ class Peach_DT_SimpleFormatTest extends PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {
-    }
-    
-    public static function createObject($arg)
-    {
-        if (is_array($arg)) {
-            return array_map("Peach_DT_SimpleFormatTest::createObject", $arg);
-        } else {
-            return new Peach_DT_SimpleFormat($arg);
-        }
     }
     
     /**
@@ -147,7 +140,7 @@ class Peach_DT_SimpleFormatTest extends PHPUnit_Framework_TestCase
             $expected = $sample->getExpected("parseTimestamp");
             $this->assertEquals($expected, $object->parseTimestamp($valid));
             try {
-                $formats[0]->parseTimestamp($invalid);
+                $object->parseTimestamp($invalid);
                 $this->fail();
             } catch (Exception $e) {}
         }
@@ -158,17 +151,12 @@ class Peach_DT_SimpleFormatTest extends PHPUnit_Framework_TestCase
      */
     public function testFormatDate()
     {
-        $d    = new Peach_DT_Date(2012, 5, 21);
-        $obj  = $this->testFormats;
-        $test = array(
-            array("20120521000000", "2012年5月21日0時0分0秒", "Y=2012 n=5 j=21 H=00 i=00 s=00"),
-            array("2012/05/21",  "2012.5.21"),
-            array("00:00:00",  "0時0分0秒")
-        );
-        for ($i = 0; $i < 3; $i ++) {
-            for ($j = 0, $len = count($test[$i]); $j < $len; $j ++) {
-                $this->assertSame($test[$i][$j], $obj[$i][$j]->formatDate($d));
-            }
+        foreach ($this->sampleList as $sample) {
+            $format     = $sample->getFormat();
+            $formatData = $sample->getFormatData("date");
+            $expected   = $formatData->getExpected();
+            $subject    = $formatData->getTime();
+            $this->assertSame($expected, $format->formatDate($subject));
         }
     }
     
@@ -177,17 +165,12 @@ class Peach_DT_SimpleFormatTest extends PHPUnit_Framework_TestCase
      */
     public function testFormatDatetime()
     {
-        $d    = new Peach_DT_Datetime(2012, 5, 21, 7, 30);
-        $obj  = $this->testFormats;
-        $test = array(
-            array("20120521073000", "2012年5月21日7時30分0秒", "Y=2012 n=5 j=21 H=07 i=30 s=00"),
-            array("2012/05/21",  "2012.5.21"),
-            array("07:30:00",  "7時30分0秒")
-        );
-        for ($i = 0; $i < 3; $i ++) {
-            for ($j = 0, $len = count($test[$i]); $j < $len; $j ++) {
-                $this->assertSame($test[$i][$j], $obj[$i][$j]->formatDatetime($d));
-            }
+        foreach ($this->sampleList as $sample) {
+            $format     = $sample->getFormat();
+            $formatData = $sample->getFormatData("datetime");
+            $expected   = $formatData->getExpected();
+            $subject    = $formatData->getTime();
+            $this->assertSame($expected, $format->formatDatetime($subject));
         }
     }
     
@@ -196,17 +179,12 @@ class Peach_DT_SimpleFormatTest extends PHPUnit_Framework_TestCase
      */
     public function testFormatTimestamp()
     {
-        $d    = new Peach_DT_Timestamp(2012, 5, 21, 7, 30, 9);
-        $obj  = $this->testFormats;
-        $test = array(
-            array("20120521073009", "2012年5月21日7時30分9秒", "Y=2012 n=5 j=21 H=07 i=30 s=09"),
-            array("2012/05/21",  "2012.5.21"),
-            array("07:30:09",  "7時30分9秒")
-        );
-        for ($i = 0; $i < 3; $i ++) {
-            for ($j = 0, $len = count($test[$i]); $j < $len; $j ++) {
-                $this->assertSame($test[$i][$j], $obj[$i][$j]->formatTimestamp($d));
-            }
+        foreach ($this->sampleList as $sample) {
+            $format     = $sample->getFormat();
+            $formatData = $sample->getFormatData("timestamp");
+            $expected   = $formatData->getExpected();
+            $subject    = $formatData->getTime();
+            $this->assertSame($expected, $format->formatTimestamp($subject));
         }
     }
 }
@@ -227,13 +205,30 @@ class Peach_DT_SimpleFormatTest_Sample
     private $parseData;
     
     /**
-     * @param string $format
-     * @param Peach_DT_SimpleFormatTest_ParseData $data
+     * @var array
      */
-    public function __construct($format, Peach_DT_SimpleFormatTest_ParseData $data)
+    private $formatDataList;
+    
+    /**
+     * @param string                           $format
+     * @param Peach_DT_SimpleFormat_ParseData  $parseData
+     * @param Peach_DT_SimpleFormat_FormatData $fDate
+     * @param Peach_DT_SimpleFormat_FormatData $fDatetime
+     * @param Peach_DT_SimpleFormat_FormatData $fTimestamp
+     */
+    public function __construct($format,
+        Peach_DT_SimpleFormatTest_ParseData  $parseData,
+        Peach_DT_SimpleFormatTest_FormatData $fDate,
+        Peach_DT_SimpleFormatTest_FormatData $fDatetime,
+        Peach_DT_SimpleFormatTest_FormatData $fTimestamp)
     {
         $this->format    = new Peach_DT_SimpleFormat($format);
-        $this->parseData = $data;
+        $this->parseData = $parseData;
+        $this->formatDataList = array(
+            "date"      => $fDate,
+            "datetime"  => $fDatetime,
+            "timestamp" => $fTimestamp,
+        );
     }
     
     /**
@@ -270,6 +265,15 @@ class Peach_DT_SimpleFormatTest_Sample
     public function getExpected($type)
     {
         return $this->parseData->getExpected($type);
+    }
+    
+    /**
+     * @param  string $type
+     * @return Peach_DT_SimpleFormat_FormatData
+     */
+    public function getFormatData($type)
+    {
+        return $this->formatDataList[$type];
     }
 }
 
@@ -331,5 +335,44 @@ class Peach_DT_SimpleFormatTest_ParseData
     public function getExpected($type)
     {
         return $this->expected[$type];
+    }
+}
+
+class Peach_DT_SimpleFormatTest_FormatData
+{
+    /**
+     * @var Peach_DT_Time
+     */
+    private $time;
+    
+    /**
+     * @var string
+     */
+    private $expected;
+    
+    /**
+     * @param Peach_DT_Time $time
+     * @param string        $expected
+     */
+    public function __construct(Peach_DT_Time $time, $expected)
+    {
+        $this->time     = $time;
+        $this->expected = $expected;
+    }
+    
+    /**
+     * @return Peach_DT_Time
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getExpected()
+    {
+        return $this->expected;
     }
 }
